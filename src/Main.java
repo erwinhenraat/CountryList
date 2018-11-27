6f72aaca4d7bbb8d4c2aafd7e4c83c3d483d4e76
erwinhenraat <e.henraat@ma-web.nl>
2018-11-27 12:49:08 +0100

    display complete list

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,16 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(getRandomCountry());
+        displayCompleteList();
     }
-    public static String getRandomCountry(){
+    private static String getRandomCountry(){
         int r = new Random().nextInt(CountryList.LIST.length-1);
        return CountryList.LIST[r];
     }
+    private static void displayCompleteList(){
+        for (String country : CountryList.LIST) {
+            System.out.println(" ++++ " +country + ".");
+        }
+    }
+
 }
